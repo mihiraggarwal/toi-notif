e0e51d9024765a1a0b71c2206a989e11fdbe3465
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2022-06-15 23:51:40 +0530

    prompt

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import webbrowser
 import bs4
 import lxml
 import time
 import json
 import requests
+import webbrowser
 from win10toast_click import ToastNotifier
 
 PATH = "https://timesofindia.indiatimes.com/"
 toaster = ToastNotifier()
+banner = '''  
+  __         .__                         __  .__  _____ 
+_/  |_  ____ |__|           ____   _____/  |_|__|/ ____\\
+\   __\/  _ \|  |  ______  /    \ /  _ \   __\  \   __\ 
+ |  | (  <_> )  | /_____/ |   |  (  <_> )  | |  ||  |   
+ |__|  \____/|__|         |___|  /\____/|__| |__||__|   
+                               \/                       
+'''
 
 def sections():
     req = requests.get(PATH).text
@@ -54,8 +62,69 @@ def notif(news):
         callback_on_click = lambda: link(news[1])
     )
 
+def prompt():
+    with open('user.json', 'w') as f:
+        heads = sections()
+        hks = list(heads.keys())
+        user = {"heads":{}, "gap":5}
+
+        print(banner)
+        print('Welcome to toi-notif!\n\n')
+        print('Along with the headlines, you may choose upto 4 of the following official TOI headings to receive news updates about.')
+        print('Just input the numbers corresponding to your choice one at a time.\n')
+
+        for i in range(1, len(heads)+1):
+            print(f'{i:>2}. {hks[i-1]}')
+        print(f'{len(heads)+1:>2}. Quit')
+
+        print('')    
+
+        i = 0
+        while i < 4:
+            try:
+                ch = int(input('> '))
+                if ch == len(heads)+1:
+                    break
+                if hks[ch-1] in user['heads']:
+                    print(f'\n{hks[ch-1]} has already been selected\n')
+                    continue
+                user['heads'][hks[ch-1]] = heads[hks[ch-1]]
+                i+=1
+            except:
+                print('\nValue not supported\n')
+
+        user['heads']['Headlines'] = PATH
+        print('\nChoices recorded! You will be notified of 5 articles from each of these headings per day.\n\n')
+
+        print('In intervals of how many minutes do you want your news? (max: 30)\n')
+
+        j = 0
+        while j < 1:
+            try:
+                intval = float(input('> '))
+                if intval > 30:
+                    print('\nThe interval must be less than 30 mins\n')
+                elif intval <= 0:
+                    print('\nValue not supported\n')
+                else:
+                    user['gap'] = intval
+                    j+=1
+            except:
+                print('\nValue not supported\n')
+
+        json.dump(user, f)
+        print('\nThank you! You are now ready to receive news updates.\n')
+
 def main():
-    pass
+    try:
+        with open('user.json', 'r') as f:
+            data = json.load(f)
+            if len(data["heads"]) == 0:
+                raise Exception
+    except:
+        prompt()
+    finally:
+        pass
 
 if __name__ == '__main__':
     main()
